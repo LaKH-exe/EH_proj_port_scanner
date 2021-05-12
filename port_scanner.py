56d3f56427e46311249b3fa01ca49fe5c2ee55c9
LaKH-exe <31774612+LaKH-exe@users.noreply.github.com>
2021-05-12 16:07:50 +0300

    add import error handler and use colored

--- port_scanner.py
+++ port_scanner.py
@@ -1,4 +1,17 @@
-import socket
+
+#from termcolor import colored
+
+try:
+    import socket
+    from termcolor import colored
+except ImportError as err:
+    print(err)
+    module = "".join(str(err).split(' ')[-1:]).strip("\'")
+    print("%s IS NOT INSTALLED. USE 'pip install %s' TO INSTALL IT " %
+          (module, module))
+
+
+
 # creating an obj of the socket
 #will use ipv4 address and tcp stream
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,9 +23,9 @@ port = int(input("[*] Enter the port\t"))
 def portscanner(port):
     # will return a 0 if no erros
     if sock.connect_ex((host, port)):
-        print("port %d is closed" % port)
+        print(colored("[-] port %d is closed" % port, "red"))
     else:
-        print("port %d is open" % port)
+        print(colored("[+] port %d is open" % port, "green"))
 
 
 portscanner(port)
