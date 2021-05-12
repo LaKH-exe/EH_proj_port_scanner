8cf90bab8f5c64fa9912788b9540f7d2af280be2
LaKH-exe <31774612+LaKH-exe@users.noreply.github.com>
2021-05-12 17:49:20 +0300

    fromat doc

--- port_scanner.py
+++ port_scanner.py
@@ -4,20 +4,13 @@
 try:
     import socket
     from termcolor import colored
+    from optparse import OptionParser
+
 except ImportError as err:
     print(err)
     module = "".join(str(err).split(' ')[-1:]).strip("\'")
     print("%s IS NOT INSTALLED. USE 'pip install %s' TO INSTALL IT " %
           (module, module))
-
-
-
-# creating an obj of the socket
-#will use ipv4 address and tcp stream
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#the host and the port we want to scan
-host = input("[*] Enter the host\t")
-port = int(input("[*] Enter the port\t"))
 
 
 def portscanner():
